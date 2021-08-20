bc2279997001b89095adb82828be5b3e0030ad94
SusithraOptisol <89243847+SusithraOptisol@users.noreply.github.com>
2021-08-20 04:07:36 -0700

    Add files via upload

---
+++ Public.py
@@ -0,0 +1,19 @@
+class Geek:
+	
+	# constructor
+	def __init__(self, name, age):
+		# public data mambers
+		self.geekName = name
+		self.geekAge = age
+
+	# public member function	
+	def displayAge(self):
+		print("Age: ", self.geekAge)
+
+
+obj = Geek("R2J", 20)
+
+# accessing public data member
+print("Name: ", obj.geekName)
+
+obj.displayAge()
